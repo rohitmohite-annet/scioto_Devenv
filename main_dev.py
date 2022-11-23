8837de956e0ba152c5e7bbaaf71d41b011a971a0
rohitmohite-annet <rohit.mohite@annet.com>
2022-11-23 13:45:21 +0530

    read the files

--- main_dev.py
+++ main_dev.py
@@ -1,3 +1,7 @@
 import pandas as pd
 data = pd.read_csv('C:\\Rohit_Data\\WORK\\Scioto_Work\\Dev_Environment\\Data\\viewIncomeStatement.csv')
 print(data.shape)
+sqft = pd.read_csv('C:\\Rohit_Data\\WORK\\Scioto_Work\\Dev_Environment\\Data\\Viewpropertyunitlease.csv',usecols=['PropertyID','Property Description','Unit Square Feet'])
+sqft.drop_duplicates(subset="PropertyID",inplace=True)
+print(sqft.shape)
+print('Unitsqft --->',sqft['Unit Square Feet'].isnull().sum())
